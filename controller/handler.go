ce067a2d6748024a5cf75c664b1ab382d43fc5b5
Franklyn Omonade <77660863+nade-harlow@users.noreply.github.com>
2021-12-26 17:25:25 +0100

    hadlers refactored

--- controller/handler.go
+++ controller/handler.go
@@ -1,1 +1,13 @@
 package controller
+
+import "github.com/gin-gonic/gin"
+
+func (s *Server) Add() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := s.Task.Add()
+		if err != nil {
+			c.JSON(500, gin.H{"error": "can't ==="})
+			return
+		}
+	}
+}
