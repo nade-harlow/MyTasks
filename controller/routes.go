5ddd09d63bcaa6c0f59099352a78bc5a421629b4
Franklyn Omonade <77660863+nade-harlow@users.noreply.github.com>
2021-12-26 17:26:20 +0100

    add route added

--- controller/routes.go
+++ controller/routes.go
@@ -2,8 +2,9 @@ package controller
 
 import "github.com/gin-gonic/gin"
 
-func Route(r *gin.Engine) {
+func (s *Server) Route(r *gin.Engine) {
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "Pinged"})
 	})
+	r.POST("/add", s.Add())
 }
