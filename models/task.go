2e738d3e4e5be8978830676110c92b759bf01e01
Franklyn Omonade <77660863+nade-harlow@users.noreply.github.com>
2021-12-26 17:32:27 +0100

    reference field added to task struct

--- models/task.go
+++ models/task.go
@@ -1,6 +1,7 @@
 package models
 
 type Task struct {
+	Reference    string `json:"reference" bson:"reference"`
 	Title        string `json:"title" bson:"title"`
 	Done         bool   `json:"done" bson:"done"`
 	CreatedAt    string `json:"created_at" bson:"created_at"`
