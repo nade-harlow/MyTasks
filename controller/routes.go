2f945e122334496b1c1038958752078c79e007fd
Franklyn Omonade <77660863+nade-harlow@users.noreply.github.com>
2021-12-28 16:53:16 +0100

    home route added

--- controller/routes.go
+++ controller/routes.go
@@ -3,9 +3,7 @@ package controller
 import "github.com/gin-gonic/gin"
 
 func (s *Server) Route(r *gin.Engine) {
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Pinged"})
-	})
+	r.GET("/", s.Home())
 	r.POST("/add", s.AddTask())
 	r.PUT("/update/:reference", s.UpdateTaskStatus())
 	r.GET("/tasks", s.GetAllTask())
